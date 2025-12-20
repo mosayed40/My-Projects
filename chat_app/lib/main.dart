87e1246236a2210e3d1103f17f49eae5a2e8f248
Mostafa Sayed <moustafa.2950@gmail.com>
2025-12-20 18:21:55 +0200

    Linking the app to Firebase

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/pages/login_page.dart';
 import 'package:chat_app/pages/register_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ScholarChat());
 }
 
@@ -17,7 +21,7 @@ class ScholarChat extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      initialRoute: 'login',
+      initialRoute: 'loginPage',
 
       routes: {
         'loginPage': (context) => LoginPage(),
