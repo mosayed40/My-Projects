0dcc003c1defbb7dceef933b98dd329f9b0605fa
Mostafa Sayed <moustafa.2950@gmail.com>
2025-12-28 19:17:07 +0200

    Provider add note cubit

--- notes/lib/cubits/add_note_cubit/add_note_cubit_cubit.dart
+++ notes/lib/cubits/add_note_cubit/add_note_cubit_cubit.dart
@@ -6,8 +6,8 @@ import 'package:notes/models/note_model.dart';
 
 part 'add_note_cubit_state.dart';
 
-class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
-  AddNoteCubitCubit() : super(AddNoteCubitInitial());
+class AddNoteCubit extends Cubit<AddNoteCubitState> {
+  AddNoteCubit() : super(AddNoteCubitInitial());
 
   addNote(NotesModel note) async {
     emit(AddNoteCubitLoading());
--- notes/lib/views/widgets/add_note_bottom_sheet.dart
+++ notes/lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/add_note_cubit/add_note_cubit_cubit.dart';
 import 'package:notes/views/widgets/custom_button.dart';
 import 'package:notes/views/widgets/custom_text_form_field.dart';
 
@@ -28,40 +30,43 @@ class _AddNoteFormState extends State<AddNoteForm> {
   String? title, subTitle;
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        children: [
-          const SizedBox(height: 32),
-          CustomTextField(
-            onSaved: (val) {
-              title = val;
-            },
-            hintText: 'Title',
-          ),
-          const SizedBox(height: 16),
-          CustomTextField(
-            onSaved: (val) {
-              subTitle = val;
-            },
-            hintText: 'Content',
-            maxLines: 5,
-          ),
-          const SizedBox(height: 64),
-          CustomButton(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                setState(() {
-                  autovalidateMode = AutovalidateMode.always;
-                });
-              }
-            },
-            text: 'Add',
-          ),
-        ],
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: Form(
+        key: formKey,
+        autovalidateMode: autovalidateMode,
+        child: Column(
+          children: [
+            const SizedBox(height: 32),
+            CustomTextField(
+              onSaved: (val) {
+                title = val;
+              },
+              hintText: 'Title',
+            ),
+            const SizedBox(height: 16),
+            CustomTextField(
+              onSaved: (val) {
+                subTitle = val;
+              },
+              hintText: 'Content',
+              maxLines: 5,
+            ),
+            const SizedBox(height: 64),
+            CustomButton(
+              onTap: () {
+                if (formKey.currentState!.validate()) {
+                  formKey.currentState!.save();
+                } else {
+                  setState(() {
+                    autovalidateMode = AutovalidateMode.always;
+                  });
+                }
+              },
+              text: 'Add',
+            ),
+          ],
+        ),
       ),
     );
   }
